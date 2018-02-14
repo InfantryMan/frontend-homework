bf8bf5b312ee8791e334a6a31a176ef9246e20f1
rmigranov <rust-skate@mail.ru>
2018-02-14 19:50:54 +0300

    Улучшил код

--- source/sort.js
+++ source/sort.js
@@ -1,42 +1,10 @@
 function sort(string) {
-    var words = string.split(" ").filter(v => v !== '');  // из одной строки с пробелами получаем массив строк
-    var i = 0;
+    let words = string.split(" ").filter(v => v !== '');  // из одной строки с пробелами получаем массив строк
+    let collator = new Intl.Collator();
+    let sortedWord = null;
 
-    return words = words.map( v => {
-        v = v.toLowerCase().split("").sort(compareLetter).join("");
-        v = v[0].toUpperCase() + v.substring(1, v.length);
-        return v;
-    }).sort(compareWord).join(" ");
-    
-}
-
-function checkYo(a, b) {    // проверка буквы ё
-    if (a.charCodeAt(0) === 1105) {
-        return b.charCodeAt(0) <= 1077 ? 1: -1;
-    }
-    if (b.charCodeAt(0) === 1105) {
-        return a.charCodeAt(0) <= 1077 ? -1: 1;
-    }
-
-    if (a.charCodeAt(0) === 1025) {
-        return b.charCodeAt(0) <= 1045 ? 1: -1;
-    }
-    if (b.charCodeAt(0) === 1025) {
-        return a.charCodeAt(0) <= 1045 ? -1: 1;
-    }
-}
-
-function compareLetter(a, b) {
-    var isYo;
-    if ( ( isYo = checkYo(a, b) ) !== undefined )
-        return isYo;
-    return ( a.charCodeAt(0) - b.charCodeAt(0) );
-
-}
-
-function compareWord(a, b) {
-    var isYo;
-    if ( ( isYo = checkYo(a, b)) !== undefined )
-        return isYo;
-    return (a > b);
+    return words.map( word => {
+        return(sortedWord = word.toLowerCase().split("").sort(collator.compare).join(""))
+        [0].toUpperCase() + sortedWord.substring(1, word.length);
+    }).sort(collator.compare).join(" ");
 }
